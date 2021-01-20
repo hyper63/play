d0882792cb7fb8785c7e96ff480afe50458491a9
Tom Wilson <twilson63@gmail.com>
2021-01-20 14:40:31 -0500

    added error handling for unauthorized requests

--- jwt.js
+++ jwt.js
@@ -1,9 +1,14 @@
 
 const jwt = require('express-jwt')
 module.exports = (app) => {
-  app.use(/^\/(data|search|storage|cache)/, jwt({
+  app.use(jwt({
     secret: process.env.SECRET, 
     algorithms: ['HS256']
-  }))
+  }).unless({path: ['/']}))
+  app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).send({ok: false, msg: 'not authorized'})
+    }
+  })
   return app
 }
